d7a76ba98551b8acd8475b94c37e6e862d4afb2c
Andy Grove <andygrove73@gmail.com>
2018-05-28 11:16:36 -0600

    Remove main method

--- octasonic/octasonic.py
+++ octasonic/octasonic.py
@@ -54,21 +54,3 @@ class Octasonic:
 
   def toggle_led(self):
     self.send(CMD_TOGGLE_LED, 0x00)
-
-def main():
-  octasonic = Octasonic(0)
-  protocol_version = octasonic.get_protocol_version()
-  firmware_version = octasonic.get_firmware_version()
-  print "Protocol v%s; Firmware v%s" % (protocol_version, firmware_version)
-  octasonic.set_sensor_count(8)
-  print "Sensor count: %s" % octasonic.get_sensor_count()
-  for x in range(0, 100):
-    octasonic.toggle_led()
-    time.sleep(0.25)
-    for i in range(0, 7):
-      print octasonic.get_sensor_reading(i),
-    print
-
-if __name__ == "__main__":
-  main()
-
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'octasonic',
     packages = ['octasonic'],
-    version = '1.0.1',
+    version = '1.0.2',
     description = 'Python library for the Octasonic breakout board',
     author = 'Andy Grove',
     author_email = 'andygrove73@gmail.com',
     url = 'https://github.com/andygrove/octasonic-python',
-    download_url = 'https://github.com/andygrove/octasonic-python/archive/1.0.1.tar.gz',
+    download_url = 'https://github.com/andygrove/octasonic-python/archive/1.0.2.tar.gz',
     keywords = ['octasonic', 'ultrasonic'],
     classifiers = [],
 )
