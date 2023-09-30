770e00e13a0064bcbcdb193032d0b8fee9b15a0e
Andy Grove <andygrove73@gmail.com>
2023-09-30 11:26:22 -0600

    Update to work with Python 3

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
+
 setup(
-    name = 'octasonic',
-    packages = ['octasonic'],
-    version = '1.0.2',
-    description = 'Python library for the Octasonic breakout board',
-    author = 'Andy Grove',
-    author_email = 'andygrove73@gmail.com',
-    url = 'https://github.com/andygrove/octasonic-python',
-    download_url = 'https://github.com/andygrove/octasonic-python/archive/1.0.2.tar.gz',
-    keywords = ['octasonic', 'ultrasonic'],
-    classifiers = [],
+    name='octasonic',
+    packages=['octasonic'],
+    version='1.0.2',
+    description='Python library for the Octasonic breakout board',
+    long_description='Python library for the Octasonic breakout board',
+    long_description_content_type='text/x-rst',
+    author='Andy Grove',
+    author_email='andygrove73@gmail.com',
+    url='https://github.com/andygrove/octasonic-python',
+    download_url='https://github.com/andygrove/octasonic-python/archive/1.0.2.tar.gz',
+    keywords=['octasonic', 'ultrasonic'],
+    classifiers=[],
 )
