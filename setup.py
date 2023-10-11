67948a0e84de7282f7a0f5c9962c7adfcc2ba4d8
Andy Grove <agrove@apache.org>
2023-10-10 19:21:26 -0600

    version 1.1

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
     name='octasonic',
     packages=['octasonic'],
-    version='1.0.2',
+    version='1.1',
     description='Python library for the Octasonic breakout board',
     long_description='Python library for the Octasonic breakout board',
     long_description_content_type='text/x-rst',
     author='Andy Grove',
     author_email='andygrove73@gmail.com',
     url='https://github.com/andygrove/octasonic-python',
-    download_url='https://github.com/andygrove/octasonic-python/archive/1.0.2.tar.gz',
+    download_url='https://github.com/andygrove/octasonic-python/archive/1.1.tar.gz',
     keywords=['octasonic', 'ultrasonic'],
     classifiers=[],
 )
